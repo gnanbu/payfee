a733b9915997d24538792f140ca68bd2242e5691
umaprasath <umaprasath.lru@gmail.com>
2014-05-14 09:27:06 +0800

    update and tag student
    Parents can tag students

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $locationProvider.html5Mode(true);
     $routeProvider.when('/',{
         templateUrl: '/payfee/partials/studentsList.html',controller: 'schoolCtrl'
     }).when('/addStudent',{
-        templateUrl: '/payfee/partials/AddStudent.html', controller: 'TypeaheadCtrl'
+        templateUrl: '/payfee/partials/AddStudent.html', controller: 'AddStudentCtrl'
     }).when('/editStudent/:id',{
         templateUrl: '/payfee/partials/AddStudent.html', controller: 'EditStudentCtrl'
     }).when('/payfee/:id',{
@@ -26,11 +26,11 @@ $locationProvider.html5Mode(true);
 
 app.run(function($rootScope, user) {
     user.init({ appId: '53441326d7e76' });
-
+    $rootScope.students= [];
+    $rootScope.o=[];
 });
 
 app.factory('FeeService',['$resource',function($resource){
-
     return $resource('http://localhost:8080/school-ws/api/school/fee',{},{
         query: {
             method: 'GET',
@@ -46,8 +46,6 @@ app.factory('SchoolService',['$resource',function($resource){
     return $resource('http://16.162.70.242:8080/payFee/api/payfeeservice/getSchool',{},{});
 }]);
 
-
-
  function PayFeeCtrl($scope,$location,$routeParams ) {
      $scope.student=$scope.students[$routeParams.id];
      $scope.payFee = function()
@@ -56,28 +54,30 @@ app.factory('SchoolService',['$resource',function($resource){
      }
 }
 
-function AddStudentCtrl ($scope,$location,FeeService ) {
+function AddStudentCtrl ($scope,$location,FeeService,$http) {
 
-     $scope.student={name:"", standard:"",school:""}
-     $scope.student.school="0";
+     $scope.student={name:"", standard:"",schoolName:"",schoolId:"", id:"",parentEmailId:"",previousStudentId:""}
+     $scope.student.schoolName="0";
      $scope.student.standard="0";
-
-     $scope.save = function()
-     {
-         $scope.students.push($scope.student);
-         $location.path('/');
-     }
-    $scope.items = {
-        "name": "Alabama",
-        "abbreviation": "AL"
-    };
     $scope.name = ''; // This will hold the selected item
     $scope.onItemSelected = function() { // this gets executed when an item is selected
-        console.log('selected=' + $scope.name);
+        console.log('selected=' + $scope.schoolId);
     };
 
+    $scope.save = function()
+    {
+      console.log("inside save"+ $scope.student.id);
+        $scope.student.parentEmailId= $scope.user.email;
+        $scope.students.push($scope.student);
+        $http({
+            method : 'POST',
+            url : 'http://localhost:8080/school-ws/api/school/fee/saveStudent',
+            //     url: 'http://16.162.70.242:8080/payFee/api/payfeeservice/updateStudentParentsEmail',
+            data : $scope.student
+        })
+        $location.path('/');
+    }
 }
-
 
 function uploadCtrl($scope,XLSXReaderService,FeeService ) {
     $scope.showPreview = false;
@@ -109,25 +109,8 @@ function uploadCtrl($scope,XLSXReaderService,FeeService ) {
         var dataArray = [];
         var jsonData;
         var iterData = $scope.sheets['Sheet1'].data;
-
-//        for (var count=0;count<iterData.length;count++)
-//        {
-//
-//       //     jsonData = {iterDat[0].split(",")};
-//            var str = "\""+iterData[count] +"\"";
-//            var schoolId=(str.split(',')[0]);
-//            var schoolName= (str.split(',')[1]);
-//
-//            var jsonData = "{schoolId:"+schoolId +"\","+"schoolName:\""+ schoolName+"}";
-//            alert(jsonData);
-//    dataArray.push(jsonData);
-//        }
-        alert(($scope.sheets['Sheet1']).data[0]);
     FeeService.save(($scope.sheets['Sheet1']).data);
-
     }
-
-
 }
 
 /*this factory is for xls upload*/
@@ -136,61 +119,48 @@ app.factory("XLSXReaderService", ['$q', '$rootScope',
         var service = function(data) {
             angular.extend(this, data);
         };
-
         service.readFile = function(file, showPreview) {
             var deferred = $q.defer();
-
             XLSXReader(file, showPreview, function(data) {
                 $rootScope.$apply(function() {
 
                     deferred.resolve(data);
-
                 });
             });
-
             return deferred.promise;
         };
-
-
         return service;
     }
 ]);
 
-function EditStudentCtrl($scope,$location,$routeParams ) {
-    $scope.student=$scope.students[$routeParams.id];
+function EditStudentCtrl($scope,$location,$routeParams,$http ) {
 
+
+    $scope.student=$scope.students[$routeParams.id];
+    console.log("inside edit"+$scope.student.name);
     $scope.save = function()
     {
-          $location.path('/');
+        console.log("inside save"+ $scope.student.previousStudentId);
+        $scope.student.parentEmailId= $scope.user.email;
+   //     $scope.students.push($scope.student);
+        $http({
+            method : 'POST',
+            url : 'http://localhost:8080/school-ws/api/school/fee/saveStudent',
+            //     url: 'http://16.162.70.242:8080/payFee/api/payfeeservice/updateStudentParentsEmail',
+            data : $scope.student
+        })
+        $location.path('/');
     }
 
 }
 
 function schoolCtrl($scope,$rootScope,FeeService,SchoolService){
-    $scope.students= [
-        {name:"Muruga",standard:"1"},
-        {name:"Aarupadaiyappa",standard:"2"}
-    ]
-
     $scope.myForm = {};
-
 // Model for your selected item
     $scope.selection;
-
-// List of choices
-    $scope.states = ['Alabama','California','Deleware'];
-
-    FeeService.get();
+  //  FeeService.get();
     var schoolList = SchoolService.query();
-
     $scope.myForm.schools = schoolList;
-        //[    {id:"0",name:"select"}].push(schoolList);
-//  /*      [
-//        {id:"0",name:"select"},
-//        {id:"1",name:"Aringnar Anna middle school,Tondiarpet"},
-//        {id:"2",name:"Kalaimagal vidyalaya, Royapuram"}
-//    ]*/
-
     $scope.myForm.standards= [
         {id:"0",name:"select"},{id:"1",name:"I"},{id:"2",name:"II"},{id:"3",name:"III"}
     ]
@@ -199,8 +169,6 @@ function schoolCtrl($scope,$rootScope,FeeService,SchoolService){
        $scope.students.splice(idx,1);
 
     }
-
-
 }
 
 function helpCtrl($scope, $http, $log, promiseTracker, $timeout){
@@ -211,9 +179,6 @@ function helpCtrl($scope, $http, $log, promiseTracker, $timeout){
         'user': 'Report a Malicious User',
         'other': 'Other'
     };
-
-
-
 
     $scope.progress = promiseTracker('progress');
     var config = {
@@ -227,7 +192,6 @@ function helpCtrl($scope, $http, $log, promiseTracker, $timeout){
         },
         tracker : 'progress'
     };
-
 
     // Form submit handler.
     $scope.submit = function(form) {
@@ -282,10 +246,7 @@ function helpCtrl($scope, $http, $log, promiseTracker, $timeout){
     };
 }
 
-
-function TypeaheadCtrl($scope, $http) {
-
-    $scope.selected = undefined;
+function TypeaheadCtrl($scope, $http,limitToFilter,$location) {
     $scope.countries = [{countryId: 0,CountryCode: "IN",CountryDesc: "India"}];
     $scope.states = [
         {StateId: 0,StateCode: "ANI",StateDesc: "Andaman and Nicobar Islands"},
@@ -342,7 +303,49 @@ function TypeaheadCtrl($scope, $http) {
         });
     };
 
-    $scope.statesWithFlags = [{"name":"Alabama","flag":"5/5c/Flag_of_Alabama.svg/45px-Flag_of_Alabama.svg.png"},{"name":"Alaska","flag":"e/e6/Flag_of_Alaska.svg/43px-Flag_of_Alaska.svg.png"},{"name":"Arizona","flag":"9/9d/Flag_of_Arizona.svg/45px-Flag_of_Arizona.svg.png"},{"name":"Arkansas","flag":"9/9d/Flag_of_Arkansas.svg/45px-Flag_of_Arkansas.svg.png"},{"name":"California","flag":"0/01/Flag_of_California.svg/45px-Flag_of_California.svg.png"},{"name":"Colorado","flag":"4/46/Flag_of_Colorado.svg/45px-Flag_of_Colorado.svg.png"},{"name":"Connecticut","flag":"9/96/Flag_of_Connecticut.svg/39px-Flag_of_Connecticut.svg.png"},{"name":"Delaware","flag":"c/c6/Flag_of_Delaware.svg/45px-Flag_of_Delaware.svg.png"},{"name":"Florida","flag":"f/f7/Flag_of_Florida.svg/45px-Flag_of_Florida.svg.png"},{"name":"Georgia","flag":"5/54/Flag_of_Georgia_%28U.S._state%29.svg/46px-Flag_of_Georgia_%28U.S._state%29.svg.png"},{"name":"Hawaii","flag":"e/ef/Flag_of_Hawaii.svg/46px-Flag_of_Hawaii.svg.png"},{"name":"Idaho","flag":"a/a4/Flag_of_Idaho.svg/38px-Flag_of_Idaho.svg.png"},{"name":"Illinois","flag":"0/01/Flag_of_Illinois.svg/46px-Flag_of_Illinois.svg.png"},{"name":"Indiana","flag":"a/ac/Flag_of_Indiana.svg/45px-Flag_of_Indiana.svg.png"},{"name":"Iowa","flag":"a/aa/Flag_of_Iowa.svg/44px-Flag_of_Iowa.svg.png"},{"name":"Kansas","flag":"d/da/Flag_of_Kansas.svg/46px-Flag_of_Kansas.svg.png"},{"name":"Kentucky","flag":"8/8d/Flag_of_Kentucky.svg/46px-Flag_of_Kentucky.svg.png"},{"name":"Louisiana","flag":"e/e0/Flag_of_Louisiana.svg/46px-Flag_of_Louisiana.svg.png"},{"name":"Maine","flag":"3/35/Flag_of_Maine.svg/45px-Flag_of_Maine.svg.png"},{"name":"Maryland","flag":"a/a0/Flag_of_Maryland.svg/45px-Flag_of_Maryland.svg.png"},{"name":"Massachusetts","flag":"f/f2/Flag_of_Massachusetts.svg/46px-Flag_of_Massachusetts.svg.png"},{"name":"Michigan","flag":"b/b5/Flag_of_Michigan.svg/45px-Flag_of_Michigan.svg.png"},{"name":"Minnesota","flag":"b/b9/Flag_of_Minnesota.svg/46px-Flag_of_Minnesota.svg.png"},{"name":"Mississippi","flag":"4/42/Flag_of_Mississippi.svg/45px-Flag_of_Mississippi.svg.png"},{"name":"Missouri","flag":"5/5a/Flag_of_Missouri.svg/46px-Flag_of_Missouri.svg.png"},{"name":"Montana","flag":"c/cb/Flag_of_Montana.svg/45px-Flag_of_Montana.svg.png"},{"name":"Nebraska","flag":"4/4d/Flag_of_Nebraska.svg/46px-Flag_of_Nebraska.svg.png"},{"name":"Nevada","flag":"f/f1/Flag_of_Nevada.svg/45px-Flag_of_Nevada.svg.png"},{"name":"New Hampshire","flag":"2/28/Flag_of_New_Hampshire.svg/45px-Flag_of_New_Hampshire.svg.png"},{"name":"New Jersey","flag":"9/92/Flag_of_New_Jersey.svg/45px-Flag_of_New_Jersey.svg.png"},{"name":"New Mexico","flag":"c/c3/Flag_of_New_Mexico.svg/45px-Flag_of_New_Mexico.svg.png"},{"name":"New York","flag":"1/1a/Flag_of_New_York.svg/46px-Flag_of_New_York.svg.png"},{"name":"North Carolina","flag":"b/bb/Flag_of_North_Carolina.svg/45px-Flag_of_North_Carolina.svg.png"},{"name":"North Dakota","flag":"e/ee/Flag_of_North_Dakota.svg/38px-Flag_of_North_Dakota.svg.png"},{"name":"Ohio","flag":"4/4c/Flag_of_Ohio.svg/46px-Flag_of_Ohio.svg.png"},{"name":"Oklahoma","flag":"6/6e/Flag_of_Oklahoma.svg/45px-Flag_of_Oklahoma.svg.png"},{"name":"Oregon","flag":"b/b9/Flag_of_Oregon.svg/46px-Flag_of_Oregon.svg.png"},{"name":"Pennsylvania","flag":"f/f7/Flag_of_Pennsylvania.svg/45px-Flag_of_Pennsylvania.svg.png"},{"name":"Rhode Island","flag":"f/f3/Flag_of_Rhode_Island.svg/32px-Flag_of_Rhode_Island.svg.png"},{"name":"South Carolina","flag":"6/69/Flag_of_South_Carolina.svg/45px-Flag_of_South_Carolina.svg.png"},{"name":"South Dakota","flag":"1/1a/Flag_of_South_Dakota.svg/46px-Flag_of_South_Dakota.svg.png"},{"name":"Tennessee","flag":"9/9e/Flag_of_Tennessee.svg/46px-Flag_of_Tennessee.svg.png"},{"name":"Texas","flag":"f/f7/Flag_of_Texas.svg/45px-Flag_of_Texas.svg.png"},{"name":"Utah","flag":"f/f6/Flag_of_Utah.svg/45px-Flag_of_Utah.svg.png"},{"name":"Vermont","flag":"4/49/Flag_of_Vermont.svg/46px-Flag_of_Vermont.svg.png"},{"name":"Virginia","flag":"4/47/Flag_of_Virginia.svg/44px-Flag_of_Virginia.svg.png"},{"name":"Washington","flag":"5/54/Flag_of_Washington.svg/46px-Flag_of_Washington.svg.png"},{"name":"West Virginia","flag":"2/22/Flag_of_West_Virginia.svg/46px-Flag_of_West_Virginia.svg.png"},{"name":"Wisconsin","flag":"2/22/Flag_of_Wisconsin.svg/45px-Flag_of_Wisconsin.svg.png"},{"name":"Wyoming","flag":"b/bc/Flag_of_Wyoming.svg/43px-Flag_of_Wyoming.svg.png"}];
+    $scope.getStudent = function(studentName) {
+        //http://localhost:8080/school-ws/api/school/fee?callback=JSON_CALLBACK &filter=US&q="+studentName
+
+        return $http.jsonp("http://localhost:8080/school-ws/api/school/fee?callback=JSON_CALLBACK &filter=US&q="+studentName).then(
+     //   return $http.jsonp("http://16.162.70.242:8080/payFee/api/payfeeservice/getStudentName?callback=JSON_CALLBACK &filter=US&q="+studentName).then(
+            function(response){
+                $scope.o=response.data;
+            return limitToFilter(response.data, 15);
+        });
+    };
+
+    $scope.save = function()
+    {
+        console.log("inside save"+ $scope.student.id);
+        $scope.student.parentEmailId= $scope.user.email;
+        $scope.students.push($scope.student);
+        $http({
+            method : 'POST',
+            url : 'http://localhost:8080/school-ws/api/school/fee/saveStudent',
+       //     url: 'http://16.162.70.242:8080/payFee/api/payfeeservice/updateStudentParentsEmail',
+            data : $scope.student
+        })
+        $location.path('/');
+    }
+
+    $scope.formatLabel = function(model) {
+        console.log("inside format"+$scope.o.length);
+        if($scope.o.length>0) {
+            for (var i = 0; i < $scope.o.length; i++) {
+                console.log("inside for" + $scope.o[i].Id);
+                if (model == $scope.o[i].Name) {
+                    $scope.student.id = $scope.o[i].Id;
+                    $scope.student.name = $scope.o[i].Name;
+                    return $scope.o[i].Name;
+                }
+            }
+        }
+        else
+        {
+            $scope.student.previousStudentId = $scope.student.id;
+            return  $scope.student.name;
+        }
+    }
 }
 
 app.controller('TypeAheadController',function($scope,dataFactory){
@@ -355,8 +358,6 @@ app.controller('TypeAheadController',function($scope,dataFactory){
     }
 });
 
-
-
 app.factory('dataFactory', function($http) {
     return {
         get: function(url) {
@@ -366,7 +367,3 @@ app.factory('dataFactory', function($http) {
         }
     };
 });
-
-
-
-
